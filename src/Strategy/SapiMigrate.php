2916ba1b64d47b07a2f9624568ebbfa4dd33f886
Ondřej Jodas <ondra.jodas@gmail.com>
2025-07-24 22:07:58 +0200

    update sapi client

--- src/Strategy/SapiMigrate.php
+++ src/Strategy/SapiMigrate.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace Keboola\AppProjectMigrateLargeTables\Strategy;
 
+use Google\Auth\FetchAuthTokenInterface;
+use Google\Cloud\Storage\StorageClient as GoogleStorageClient;
+use GuzzleHttp\Utils;
 use Keboola\AppProjectMigrateLargeTables\Config;
 use Keboola\AppProjectMigrateLargeTables\MigrateInterface;
 use Keboola\AppProjectMigrateLargeTables\StorageModifier;
 use Keboola\StorageApi\Client;
 use Keboola\StorageApi\ClientException;
 use Keboola\StorageApi\Options\FileUploadOptions;
+use Keboola\StorageApi\Options\GetFileOptions;
 use Keboola\Temp\Temp;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 class SapiMigrate implements MigrateInterface
 {
@@ -94,7 +99,16 @@ class SapiMigrate implements MigrateInterface
             ->setFederationToken(true)
             ->setFileName($sourceTableInfo['id'])
         ;
-        if ($sourceFileInfo['isSliced'] === true) {
+        if ($sourceFileInfo['isSliced'] === true && $sourceFileInfo['provider'] === 'gcp') {
+            $this->migrateSlicedGcsFiles(
+                $sourceFileId,
+                $sourceTableInfo,
+                $config->preserveTimestamp(),
+                $tmp,
+            );
+            $tmp->remove();
+            return;
+        } elseif ($sourceFileInfo['isSliced'] === true) {
             $optionUploadedFile->setIsSliced(true);
 
             $this->logger->info(sprintf('Downloading table %s', $sourceTableInfo['id']));
@@ -169,5 +183,119 @@ class SapiMigrate implements MigrateInterface
             );
         }
         return $listTables;
+    }
+
+    private function migrateSlicedGcsFiles(
+        int $fileId,
+        array $tableInfo,
+        bool $preserveTimestamp,
+        Temp $tmpFolder,
+    ): void {
+        $fileInfo = $this->sourceClient->getFile(
+            $fileId,
+            (new GetFileOptions())->setFederationToken(true),
+        );
+
+        $bucket = $fileInfo['gcsPath']['bucket'];
+        $gcsClient = $this->getGcsClient($fileId);
+        $retBucket = $gcsClient->bucket($bucket);
+        $manifestObject = $retBucket->object($fileInfo['gcsPath']['key'] . 'manifest')->downloadAsString();
+
+        /** @var array{"entries": string[]} $manifest */
+        $manifest = Utils::jsonDecode($manifestObject, true);
+        $chunks = array_chunk((array) $manifest['entries'], 50);
+
+        $optionUploadedFile = new FileUploadOptions();
+        $optionUploadedFile
+            ->setFederationToken(true)
+            ->setFileName($tableInfo['id'])
+            ->setIsSliced(true)
+        ;
+
+        foreach ($chunks as $chunkKey => $chunk) {
+            $this->logger->info(sprintf('Migrating %s chunk', $chunkKey));
+            $slices = [];
+            // refresh credentials for each chunk
+            $gcsClient = $this->getGcsClient($fileId);
+            $retBucket = $gcsClient->bucket($bucket);
+            /** @var array{"url": string} $entry */
+            foreach ($chunk as $entry) {
+                $this->logger->info(sprintf('Migrating %s file.', $entry['url']));
+                $slices[] = $destinationFile = $tmpFolder->getTmpFolder() . '/' . basename($entry['url']);
+
+                $sprintf = sprintf(
+                    '/%s/',
+                    $fileInfo['gcsPath']['bucket'],
+                );
+                $blobPath = explode($sprintf, $entry['url']);
+                $retBucket->object($blobPath[1])->downloadToFile($destinationFile);
+            }
+
+            $destinationFileId = $this->targetClient->uploadSlicedFile($slices, $optionUploadedFile);
+
+            $this->targetClient->writeTableAsyncDirect(
+                $tableInfo['id'],
+                [
+                    'name' => $tableInfo['name'],
+                    'dataFileId' => $destinationFileId,
+                    'columns' => $tableInfo['columns'],
+                    'useTimestampFromDataFile' => $preserveTimestamp,
+                ],
+            );
+
+            $fs = new Filesystem();
+            foreach ($slices as $slice) {
+                $fs->remove($slice);
+            }
+        }
+    }
+
+    private function getGcsClient(int $fileId): GoogleStorageClient
+    {
+        $fileInfo = $this->sourceClient->getFile(
+            $fileId,
+            (new GetFileOptions())->setFederationToken(true),
+        );
+        $gcsCredentials = $fileInfo['gcsCredentials'];
+        $options = [
+            'credentials' => [
+                'access_token' => $gcsCredentials['access_token'],
+                'expires_in' => $gcsCredentials['expires_in'],
+                'token_type' => $gcsCredentials['token_type'],
+            ],
+            'projectId' => $gcsCredentials['projectId'],
+        ];
+
+        $fetchAuthToken = $this->getAuthTokenClass($options['credentials']);
+        return new GoogleStorageClient([
+            'projectId' => $options['projectId'],
+            'credentialsFetcher' => $fetchAuthToken,
+        ]);
+    }
+
+    private function getAuthTokenClass(array $credentials): FetchAuthTokenInterface
+    {
+        return new class ($credentials) implements FetchAuthTokenInterface {
+
+            public function __construct(
+                private array $creds,
+            ) {
+            }
+
+            public function fetchAuthToken(?callable $httpHandler = null)
+            {
+                return $this->creds;
+            }
+
+            public function getCacheKey()
+            {
+                return '';
+            }
+
+            public function getLastReceivedToken()
+            {
+                return $this->creds;
+            }
+        };
     }
 }
